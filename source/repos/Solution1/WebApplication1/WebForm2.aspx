90ac318bd00a16e1fa58ad492d0c8722e1b5a41f
k.hashimoto <go.for.broke666@gmail.com>
2020-06-14 14:01:10 +0900

    手本のプログラムを追加

---
+++ source/repos/Solution1/WebApplication1/WebForm2.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication1.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+    <form id="form2" runat="server">
+        <div class="container mt-3">
+
+            <h1 class="text-center">学生検索サービス</h1>
+
+            <div class="m-5">
+                <p class="text-center font-weight-bold">検索フォーム</p>
+                <div class="input-group">
+                    <div class="input-group-append">
+                        <span class="btn btn-dark">学生番号</span>
+                    </div>
+                    <input class="form-control" name="number" placeholder="半角数字で入力して下さい。" maxlength="3" type="text" value="<%=(Number == 0) ? "" : Number.ToString() %>"/>
+                    <div class="input-group-append">
+                        <span class="btn btn-dark">学年</span>
+                    </div>
+                    <select name="age" class="custom-select">
+                        <option value="0" <% if(Age == 0) { %> selected="selected" <% } %>>-----</option>
+                        <option value="1" <% if(Age == 1) { %> selected="selected" <% } %>>1</option>
+                        <option value="2" <% if(Age == 2) { %> selected="selected" <% } %>>2</option>
+                        <option value="3" <% if(Age == 3) { %> selected="selected" <% } %>>3</option>
+                    </select>
+                    <div class="input-group-append">
+                        <span class="btn btn-dark">並び替え</span>
+                    </div>
+                    <select name="order" class="custom-select">
+                        <option value="0" <% if(Order == 0) { %> selected="selected" <% } %>>-----</option>
+                        <option value="1" <% if(Order == 1) { %> selected="selected" <% } %>>背が高い順</option>
+                        <option value="2" <% if(Order == 2) { %> selected="selected" <% } %>>背が低い順</option>
+                        <option value="3" <% if(Order == 3) { %> selected="selected" <% } %>>体重が重い順</option>
+                        <option value="4" <% if(Order == 4) { %> selected="selected" <% } %>>体重が軽い順</option>
+                    </select>
+                </div>
+                <div class="mt-3 text-center">
+                    <input name="search" type="submit" class="btn btn-success" value="検　索" />
+                </div>
+            </div>
+
+            <% 
+                if (StudentsList.Any())
+                { 
+            %>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>番号</th>
+                        <th>学年</th>
+                        <th>名前</th>
+                        <th>身長</th>
+                        <th>体重</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <%
+                        foreach(var item in StudentsList)
+                        {
+                    %>
+                    <tr>
+                        <td><%=item.number %></td>
+                        <td><%=item.age %></td>
+                        <td><%=item.name %></td>
+                        <td><%=item.tall %></td>
+                        <td><%=item.wait %></td>
+                    </tr>
+                    <%
+                        }
+                    %>
+                </tbody>
+            </table>
+            <% 
+                }
+                else
+                {
+            %>
+            <span class="font-weight-bold text-danger">対象の学生はいません。</span>
+            <%
+                }
+            %>
+        </div>
+    </form>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ source/repos/Solution1/WebApplication1/WebForm2.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        /// <summary>
+        /// 学生情報リスト
+        /// </summary>
+        protected List<student> StudentsList;
+
+        protected int Number;
+        protected int Age;
+        protected int Order;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Form["search"] != null)
+            {
+                //検索押されている
+                int number = 0;
+                int.TryParse(Request.Form["number"], out number);
+                int age = int.Parse(Request.Form["age"]);
+                int order = int.Parse(Request.Form["order"]);
+                NameValueCollection name = System.Web.HttpUtility.ParseQueryString(string.Empty);
+                if (number > 0)
+                {
+                    name["number"] = number.ToString();
+                }
+                if (age > 0)
+                {
+                    name["age"] = age.ToString();
+                }
+                if (order > 0)
+                {
+                    name["order"] = order.ToString();
+                }
+                Response.Redirect(Request.Url.AbsolutePath + (name.ToString() == string.Empty ? "" : "?" + name.ToString()));
+                return;
+            }
+
+            StudentsList = GetStudents();
+
+            if (int.TryParse(Request.QueryString["number"], out Number))
+            {
+                if (Number > 0)
+                {
+                    StudentsList = StudentsList.Where(x => x.number == Number).ToList();
+                }
+            }
+
+            if (int.TryParse(Request.QueryString["age"], out Age))
+            {
+                if (Age > 0)
+                {
+                    StudentsList = StudentsList.Where(x => x.age == Age).ToList();
+                }
+            }
+
+            if (int.TryParse(Request.QueryString["order"], out Order))
+            {
+                switch (Order)
+                {
+                    case 1:
+                        StudentsList = StudentsList.OrderByDescending(x => x.tall).ToList();
+                        break;
+                    case 2:
+                        StudentsList = StudentsList.OrderBy(x => x.tall).ToList();
+                        break;
+                    case 3:
+                        StudentsList = StudentsList.OrderByDescending(x => x.wait).ToList();
+                        break;
+                    case 4:
+                        StudentsList = StudentsList.OrderBy(x => x.tall).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //var list = GetTable("SELECT * FROM userdate");
+        }
+
+        /// <summary>
+        /// 学生情報用クラス
+        /// </summary>
+        protected class student
+        {
+            /// <summary>
+            /// 学生番号
+            /// </summary>
+            public int number { get; set; }
+            /// <summary>
+            /// 学年
+            /// </summary>
+            public int age { get; set; }
+            /// <summary>
+            /// 名前
+            /// </summary>
+            public string name { get; set; }
+            /// <summary>
+            /// 身長
+            /// </summary>
+            public int tall { get; set; }
+            /// <summary>
+            /// 体重
+            /// </summary>
+            public int wait { get; set; }
+        }
+
+        /// <summary>
+        /// 学生情報のリストを取得する
+        /// </summary>
+        /// <returns></returns>
+        protected List<student> GetStudents()
+        {
+            //番号
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+            //学年
+            int[] schoolAges = new int[] { 1, 1, 2, 2, 3, 3 };
+            //名前
+            string[] names = new string[] { "カトウ", "ハヤシ", "ムカイ", "タンゲ", "イトウ", "サトウ" };
+            //身長
+            int[] talls = new int[] { 150, 162, 155, 161, 173, 149 };
+            //体重
+            int[] waits = new int[] { 60, 57, 62, 85, 47, 53 };
+
+            var list = new List<student>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                student student = new student();
+                student.number = numbers[i];
+                student.age = schoolAges[i];
+                student.name = names[i];
+                student.tall = talls[i];
+                student.wait = waits[i];
+                list.Add(student);
+            }
+            return list;
+        }
+    }
+}
